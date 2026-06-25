8add2c1711260ebacd57272e3f02646631cd99f4
dhairya-shah13 <shah.dhairya.p13@gmail.com>
2026-06-25 11:55:02 +0530

    Swoosh!

--- apps/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'core'
-
-urlpatterns = [
-    path('', views.HomeView.as_view(), name='home'),
-    path('rf-sports/', views.RFSportsView.as_view(), name='rf_sports'),
-]
--- apps/products/api_urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import api_views
-
-urlpatterns = [
-    path('search/', api_views.search_products, name='product_search'),
-]
